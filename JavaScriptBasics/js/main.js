7cde2acfcb7711b83a92e9606f20e3bcbde9cfe6
teruMac <tehiraga29738mic@hotmail.com>
2020-01-05 23:17:31 +0900

    Start Object lesson

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,6 +1,11 @@
 'use strict';
+{
+  const score = [80, 90, 60, 70];
+  // score.push(50, 30);
+  // score.shift();
+  score.splice(1, 1, 40, 50);
 
-function f() {
-  const x = 1;
-  console.log(x);
+  for (let i = 0; i < score.length; i++) {
+    console.log(`Score ${i}: ${score[i]}`);
+  }
 }
