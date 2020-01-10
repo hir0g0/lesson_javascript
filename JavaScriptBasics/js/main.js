9425d9a25b467def8964824cd3742fb208b03849
teruMac <tehiraga29738mic@hotmail.com>
2020-01-10 20:41:16 +0900

    complete JavaScript Object lessons

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,11 +1,45 @@
 'use strict';
 {
-  // alert('hello');
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
 
-  const answer = confirm('Realy delete?');
-  if (answer) {
-    console.log('delete');
-  } else {
-    console.log('canceled');
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+    static showInfo() {
+      console.log('Post class version 1.0.0');
+    }
   }
+
+  class SponsoredPost extends Post {
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      super.show();
+      console.log(`...sponsored by ${this.sponsor}`);
+    }
+  }
+
+
+  const posts = [
+    new Post('studying JavaScript'),
+    new Post('programming is so funny!'),
+    new SponsoredPost('master 3 mitutes', 'dotinstall'),
+  ];
+
+  posts[2].show();
+  posts[2].like();
+
 }
